c3b7176ded6b78284df4528e9646970a1876300a
Rebecca Thomson <147430406+Zekkers01@users.noreply.github.com>
2025-03-13 19:06:25 -0500

    Add an error

--- dberror.h
+++ dberror.h
@@ -14,10 +14,10 @@ typedef int RC;
 #define RC_FILE_HANDLE_NOT_INIT 2
 #define RC_WRITE_FAILED 3
 #define RC_READ_NON_EXISTING_PAGE 4
-//added error codes for more specificity in debugging user errors
 #define RC_FILE_ALREADY_EXISTS 5
 #define RC_ERROR 6
 #define RC_READ_FAILED 7
+#define RC_BUFFER_POOL_HAS_PINNED_PAGES 8
 #define RC_APPEND_FAILED 9
 
 #define RC_RM_COMPARE_VALUE_OF_DIFFERENT_DATATYPE 200
@@ -26,6 +26,8 @@ typedef int RC;
 #define RC_RM_NO_MORE_TUPLES 203
 #define RC_RM_NO_PRINT_FOR_DATATYPE 204
 #define RC_RM_UNKOWN_DATATYPE 205
+#define RC_RM_ERROR 206
+#define RC_RM_RECORD_INVALID 210
 
 #define RC_IM_KEY_NOT_FOUND 300
 #define RC_IM_KEY_ALREADY_EXISTS 301
